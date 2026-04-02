c4b552736c006b6cbdd33e57cde48fa3b3f50b89
kdy0618 <kdy0618@suwon.ac.kr>
2026-04-02 09:51:00 +0900

    과제1 코딩과 테스트 완료 Label, TextBox, Button을 적절히 배치하여 UI 구성 TextBox의 Placeholder 속성과 passwordChar속성을 활용하여 아이디와 패스워드 TextBox의 입력 힌트를 회색으로 표시 및 패스워드 입력 숨기기 로그인 성공 여부를 메시지박스로 출

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,12 +29,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lbl_title = new Label();
+            txt_id = new TextBox();
+            txt_password = new TextBox();
+            btn_login = new Button();
+            SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            lbl_title.AutoSize = true;
+            lbl_title.Font = new Font("맑은 고딕", 30F);
+            lbl_title.Location = new Point(153, 47);
+            lbl_title.Name = "lbl_title";
+            lbl_title.Size = new Size(245, 106);
+            lbl_title.TabIndex = 0;
+            lbl_title.Text = "Login";
+            // 
+            // txt_id
+            // 
+            txt_id.Font = new Font("맑은 고딕", 15F);
+            txt_id.Location = new Point(75, 187);
+            txt_id.Name = "txt_id";
+            txt_id.PlaceholderText = "아이디";
+            txt_id.Size = new Size(429, 61);
+            txt_id.TabIndex = 1;
+            // 
+            // txt_password
+            // 
+            txt_password.Font = new Font("맑은 고딕", 15F);
+            txt_password.Location = new Point(75, 306);
+            txt_password.Name = "txt_password";
+            txt_password.PasswordChar = '*';
+            txt_password.PlaceholderText = "비밀번호";
+            txt_password.Size = new Size(429, 61);
+            txt_password.TabIndex = 2;
+            // 
+            // btn_login
+            // 
+            btn_login.Font = new Font("맑은 고딕", 12F);
+            btn_login.Location = new Point(192, 421);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(150, 61);
+            btn_login.TabIndex = 3;
+            btn_login.Text = "로그인";
+            btn_login.UseVisualStyleBackColor = true;
+            btn_login.Click += btn_login_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(590, 608);
+            Controls.Add(btn_login);
+            Controls.Add(txt_password);
+            Controls.Add(txt_id);
+            Controls.Add(lbl_title);
+            Name = "Form1";
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl_title;
+        private TextBox txt_id;
+        private TextBox txt_password;
+        private Button btn_login;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,31 @@ namespace LoginScreen
 {
     public partial class Form1 : Form
     {
+        // 컨트롤 변수 선언
+        TextBox txtId, txtPassword;
+        Button btnLogin;
+
+        String id = "admin";
+        String password = "password";
         public Form1()
         {
             InitializeComponent();
+            // 생성자에서 컨트롤 변수 초기화
+            txtId = txt_id;
+            txtPassword = txt_password;
+            btnLogin = btn_login;
         }
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if(txtId.Text == id && txtPassword.Text == password)
+            {
+                MessageBox.Show("로그인 성공");
+            }
+            else
+            {
+                MessageBox.Show("아이디 또는 비밀번호가 다릅니다");
+            }
+        }
+        
     }
 }
